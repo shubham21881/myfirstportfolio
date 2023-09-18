0af4d1caaf69f4cd64a8669b4a30bd9322bdf0c0
shubham mourya <krishanmourya.sh@gmail.com>
2023-09-17 20:51:44 -0700

    my personal portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings: ["frontend Developer","web developer","backend Developer"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop:true
+})
